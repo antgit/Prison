bac78ac67a69228beea6924a04bc30fc69f4445d
Anton Ilin <anton@newdayatwork.com>
2015-02-24 20:59:59 +0100

    Circles calculation

--- Prison/Prison.cs
+++ Prison/Prison.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Prison
 {
 	public class Prison
 	{
-		public const int BoxesCount = 100;
+		public const int PrisonerCount = 100;
+		public const int NumbersOfAttemptsToOpenBox = PrisonerCount/2;
+
 		private readonly Box[] _boxes;
 
 		public Prison()
 		{
-			_boxes = new Box[BoxesCount];
+			_boxes = new Box[PrisonerCount];
 
-			for (int index = 0; index < _boxes.Length; index++)
+			for (var index = 0; index < _boxes.Length; index++)
 			{
 				_boxes[index] = new Box();
 			}
@@ -19,9 +23,9 @@ namespace Prison
 
 		public void Initialize()
 		{
-			var numbers = new bool[BoxesCount];
+			var numbers = new bool[PrisonerCount];
 
-			var rnd = new Random();
+			var random = new Random((int)DateTime.Now.Ticks);
 
 			foreach (var box in _boxes)
 			{
@@ -29,7 +33,7 @@ namespace Prison
 
 				while(number == null)
 				{
-					var randomNumber = rnd.Next(BoxesCount);
+					var randomNumber = random.Next(PrisonerCount);
 
 					if (numbers[randomNumber] == false)
 					{
@@ -40,6 +44,33 @@ namespace Prison
 
 				box.PrisonerId = number.Value;
 			}
+		}
+
+		public List<List<Box>> GetCircles()
+		{
+			var circles = new List<List<Box>>();
+
+			for (var number = 0; number < PrisonerCount; number++)
+			{
+				var box = _boxes[number];
+
+				if (circles.Any(c => c.Contains(box)))
+				{
+					continue;
+				}
+
+				var circle = new List<Box> {box};
+
+				while (box.PrisonerId != number)
+				{
+					box = _boxes[box.PrisonerId];
+					circle.Add(box);
+				}
+
+				circles.Add(circle);
+			}
+
+			return circles;
 		}
 	}
 }
--- Prison/Program.cs
+++ Prison/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Prison
+﻿using System;
+using System.Linq;
+
+namespace Prison
 {
 	class Program
 	{
@@ -6,6 +9,20 @@
 		{
 			var prison = new Prison();
 			prison.Initialize();
+
+			var circles = prison.GetCircles();
+
+			Console.WriteLine("Total number of prisoners: {0}.", Prison.PrisonerCount);
+			Console.WriteLine("Number of circles: {0}.", circles.Count);
+
+			foreach (var circle in circles)
+			{
+				Console.WriteLine(circle.Count);
+			}
+
+			Console.WriteLine(circles.Any(c => c.Count > Prison.NumbersOfAttemptsToOpenBox)
+				                  ? "Prisoners can't escape."
+				                  : "Prisoners can escape.");
 		}
 	}
 }
